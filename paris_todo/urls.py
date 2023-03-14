e7b191e19465df7b256e13a2f9ccc460598399b8
stokeydev <bbektas.dev@gmail.com>
2023-03-14 17:08:17 +0000

    django standstill

--- paris_todo/urls.py
+++ paris_todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
+from todo.views import get_todo_list, add_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
+    path('', get_todo_list, name='get_todo_list'),
+    path('add', add_item, name='add')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
+from .models import Item
 # Create your views here.
 
-def say_hello(response):
-    return HttpResponse(("Hello Saturn!"))
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+def add_item(request):
+    
+    return render(request, 'todo/add_item.html')
